2c3104c828eaa1852acdd9275a316970bcb39de7
Ardyn <lemanfoetra@gmail.com>
2025-12-16 20:40:07 +0700

    create table incomes

---
+++ database/migrations/2025_12_16_120845_create_incomes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateIncomesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('incomes', function (Blueprint $table) {
+            $table->id();
+            $table->date('income_date');
+            $table->string('source');
+            $table->double('amount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('incomes');
+    }
+}
---
+++ database/migrations/2025_12_16_121059_add_user_to_incomes.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUserToIncomes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('incomes', function (Blueprint $table) {
+            $table->unsignedBigInteger('id_users')->after('id');
+            $table->foreign('id_users')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('incomes', function (Blueprint $table) {
+            //
+        });
+    }
+}
